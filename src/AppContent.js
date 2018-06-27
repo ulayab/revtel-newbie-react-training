cf689e4d75d3dde6147439d11385fcdae90e50ad
ulayab <gary0626@gmail.com>
2018-06-27 16:49:51 +0800

    fix bug

--- src/AppContent.js
+++ src/AppContent.js
@@ -34,7 +34,9 @@ export default class AppContent extends Component {
 			<div class="flex">
 			<div>
 				<h2>台北市公園搜尋系統</h2>
-				<UserSearch onChange={(text) => this.setState({ filterInputs : text})} />
+				<UserSearch onChange={filterInputs =>
+                this.setState({ filterInputs: filterInputs })
+           		}/>
 				<ParkItems 
 					isFetching={isFetching}
 					parkList={parkList}
@@ -50,12 +52,12 @@ export default class AppContent extends Component {
 
 class UserSearch extends Component {
 	constructor(props) {
-		super(props)
+		super(props);
 		this.state = {
 			userInput1: '',
             userInput2: '',
             userInput3: '',
-		}
+		};
 
 		this.handleInputChange1 = this.handleInputChange1.bind(this)
 		this.handleInputChange2 = this.handleInputChange2.bind(this)
@@ -64,67 +66,78 @@ class UserSearch extends Component {
 	
 	render() {
 		let { userInput1,userInput2,userInput3} = this.state;
-	
-		return (
+		console.log("有人輸入了 \""+userInput1+userInput2+userInput3 +"\"")
+		
+		return(
+			
 			<div>
+				
 			<input style={styles.inputStyle}
-				type='text'
+				type="text"
 				placeholder={'請輸入欲查詢公園 (如：二二八和平公園)'}
 				value={userInput1}
 				onChange={this.handleInputChange1}
 			/>
 			<input style={styles.inputStyle}
-				type='text'
+				type="text"
 				placeholder={'請輸入欲查詢之內容'}
 				value={userInput2}
 				onChange={this.handleInputChange2}
 			/>
 			<input style={styles.inputStyle}
-				type='text'
+				type="text"
 				placeholder={'請輸入欲查詢之最早建造年份'}
 				value={userInput3}
 				onChange={this.handleInputChange3}
 			/>
 			</div>
-		)
+		
+		);
+	
 	}
 
 	handleInputChange1(evt) {
-		this.setState({ userInputs: evt.target.value });
+		this.setState({ userInput1: evt.target.value });
 
 		if (this.props.onChange) {
-			this.props.onChange(evt.target.value, this.state.userInputs, this.state.userInputs)//自動排進33行的text1,text2
+			console.log("11111111111111111111111111")
+			this.props.onChange([evt.target.value, this.state.userInput2, this.state.userInput3]); //自動排進33行的text1,text2
 		}
 	}
 	handleInputChange2(evt2) {
-		this.setState({ userInputs: evt2.target.value});
+		this.setState({ userInput2: evt2.target.value});
 
 		if (this.props.onChange) {
-			this.props.onChange(this.state.userInputs, evt2.target.value, this.state.userInputs)
+			console.log("222222222222222222222222222")
+			this.props.onChange([this.state.userInput1, evt2.target.value, this.state.userInput3]);
 		}
 	}
 	handleInputChange3(evt3) {
-		this.setState({ userInputs: evt3.target.value});
+		this.setState({ userInput3: evt3.target.value});
 
 		if (this.props.onChange) {
-			this.props.onChange(this.state.userInputs, this.state.userInputs, evt3.target.value)
+			console.log("3333333333333333333333333333")
+			this.props.onChange([this.state.userInput1, this.state.userInput2, evt3.target.value]);
 		}
 	}
-	
+
 }
+
 
 const ParkItems = ({ isFetching, parkList, filterInputs}) => {
 	if (isFetching) {
 		return <div>Fetching ...</div>
 	}
-	// const hasSubString = (src,sub) => src.indexOf(sub) !== -1;
-	// [f1, f2]= filterInputs;
+
+
+	 //const hasSubString = (src,sub) => src.indexOf(sub) !== -1;
+	 //const [f1, f2, f3] = filterInputs;
 
 	function filterLogic(parkList, filterInputs) {
 
 		let ret = []
 
-		function f1( list, text){ //第一個filter function(Name,ParkName)
+		function f1( list, text){ //第一個filter function(Name、ParkName)
 			let r = []
 			for(let i=0; i<list.length;i++){
 				if(list[i].Name.indexOf(text)!==-1 || list[i].ParkName.indexOf(text)!==-1){
@@ -142,42 +155,72 @@ const ParkItems = ({ isFetching, parkList, filterInputs}) => {
 			}
 			return r
 		}
-		function f3( list, text){
-			let r = []
-			if(text===''){
+		function f3( list, text){ //第三個filter function(YearBuilt)
+			if (text === '') {
 				return list
 			}
-			for(let i=0; i<list.length;i++){
-				if(funcYearBuilt(list[i],text)){
+			let r = []
+			for(let i=0 ; i<list.length ; i++){
+				// if(funcYearBuilt(list[i],text)){
+				// 	r.push(list[i])
+				// }
+				if(checkIfMoreThanNum(list[i],text)){
 					r.push(list[i])
 				}
 			}
 			return r
 		}
 //------------------------------------------------------------------------------//
-		function funcYearBuilt( inputY, _yearBuilt){
-			let t = inputY.YearBuilt,
-			yearBuilt = parseInt(_yearBuilt)
-
-			// tranlate api park data YearBuilt to normal number (year)
-			function translateAPIDateToNumber(text) {
-				//console.log(`... '${text}', ${yearBuilt}`)
-
-				// TODO
-				if (text === '') {
-					return -1
-				} else {
-					return parseInt(text.split('-')[0])
+			function checkIfMoreThanNum(parkDetail, _yearBuilt){
+				let t = parkDetail.YearBuilt;
+				let yearBuilt = parseInt(_yearBuilt); //把第一個參數變成字串、解析它、再回傳整數或是 NaN 
+				console.log("#####yearBuilt的數字為:" + yearBuilt);
+				function translateAPIDateToNumber(text) {
+				//	var  a = text.match(/[0-9]+/);
+					if (text === '') {
+						return -1;
+	            	}else{
+						console.log("@@text的數字為:" + text);
+						return text.match(/[0-9]+/);
+					}
 				}
-
-			}
-
-			let y = translateAPIDateToNumber(t)
-
+				let y = translateAPIDateToNumber(t)
+				if( y> yearBuilt){
+					console.log("正確要return的數字為:" + t);
+					return t 
+				}
 			// compare(park data YearBuilt to normal number, yearBuilt)
-			return y > yearBuilt
+		//	return y > yearBuilt
+			}
+			//function AC
+			
 
-		}
+
+
+
+		// function funcYearBuilt( inputY, _yearBuilt){
+		// 	let t = inputY.YearBuilt,
+		// 	yearBuilt = parseInt(_yearBuilt)
+
+		// 	// tranlate api park data YearBuilt to normal number (year)
+		// 	function translateAPIDateToNumber(text) {
+		// 		//console.log(`... '${text}', ${yearBuilt}`)
+
+		// 		// TODO
+		// 		if (text === '') {
+		// 			return -1
+		// 		} else {
+		// 			return parseInt(text.split('-')[0])
+		// 		}
+
+		// 	}
+
+		// 	let y = translateAPIDateToNumber(t)
+
+		// 	// compare(park data YearBuilt to normal number, yearBuilt)
+		// 	return y > yearBuilt
+
+		// }
 //------------------------------------------------------------------------------//
 		let ret1 = f1(parkList, filterInputs[0]);
 		let ret2 = f2(ret1, filterInputs[1]);
